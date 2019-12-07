8f897d7f30dbcc8f7a7a5cf7e89ad5eed1bd329e
K G <katrinity1@yahoo.com>
2019-12-06 23:40:20 -0800

    updated contact page

--- contact.php
+++ contact.php
@@ -1,3 +1,23 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="katrinity1@yahoo.com";
+    $name=$_POST["name"];
+    $email=$_POST["email"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $name\nEmail: $email\n\n$message";
+
+    mail($recipient, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
+
+
+
+
 <!doctype html>
 <html lang="en">
 
@@ -49,11 +69,11 @@
       <div class="col-md-8">
         <div class="containerMain">
           <header>
-            <h2>Contact me</h2>
+            <h2>Contact</h2>
           </header>
           <hr class="mt-3 mb-3">
 
-          <!--<div class="contact-form">
+          <div class="contact-form">
             <div class="form-group">
               <label class="control-label col-sm-2" for="name">Name:</label>
               <div class="col-sm-12">
@@ -81,51 +101,23 @@
         </div>
       </div>
     </div>
-  </div>-->
+  </div>
 
-          <div class="row text-center text-lg-left">
+  <div class="footer">
+    <div class="row">
+      <div class="col-md-12"></div>
+    </div>
+  </div>
 
-            <div class="col-lg-4 col-md-6 col-8">
-              <a href="https://github.com/katrinity" class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src="assets/images/github.jpg" alt="GitHub">
-                <div class="text-block text-center">
-
-                </div>
-              </a>
-            </div>
-            <div class="col-lg-4 col-md-6 col-8">
-              <a href="#" class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src="assets/images/link.jpg" alt="LinkedIn">
-                <div class="text-block text-center">
-
-                </div>
-              </a>
-            </div>
-            <div class="col-lg-4 col-md-6 col-8">
-              <a href="mailto:katrinity1@yahoo.com" class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src="assets/images/email.jpg" alt="email">
-                <div class="text-block text-center">
-
-                </div>
-              </a>
-            </div>
-
-
-            <div class="footer">
-              <div class="row">
-                <div class="col-md-12"></div>
-              </div>
-            </div>
-
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-              integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-              crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-              integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-              crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-              integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-              crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
 </body>
 
 </html>
